578c721fb8b8437142b6cb295a546eee48669c25
lynkim12 <lynkim12@gmail.com>
2023-10-29 17:00:54 +0900

    navigator

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,36 @@ import React from 'react';
 import './App.css';
 import './style.css';
 import logo from './logo.png';
+import Navigator from './Navigator';
 
 
 function App() {
   return (
-    <div className='nav'>
-      <img className='logo' src={logo} />
-
-    <div className='menu'>
-      <div className='slectedMenu'>
-        Works
+    <>
+    <Navigator />
+      <div className='contentslist'>
+        <div className='contents'>
+            <div className='content'>
+              <img className='thumbnail' />
+              <div className='contentsText'>
+                <div className='subtitle'>
+                  Karrot  
+                </div>
+                <div className='title'>
+                  New Ads Platform for Expert
+                </div>
+                <div className='description'>
+                  Designing an entirely new advertising platform for Mid- to Large-scale Advertisers
+                </div>
+              </div>
+              
+            </div>
+        </div>
+        <div className='contents'>
+            contents
+        </div>  
       </div>
-      <div className='defaultMenu'>
-        Writing
-      </div>
-      <div className='defaultMenu'>
-        About
-      </div>
-    </div>
-      
-    </div>
-
+    </>
   );
 }
 
