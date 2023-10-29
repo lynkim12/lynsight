12083e0a04ca59c0324a873f560198b894be1dc1
lynkim12 <lynkim12@gmail.com>
2023-10-29 16:18:48 +0900

    Implement Navigation Bar UI of Lynsight

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import logo from './logo.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='nav'>
+      <img className='logo' src={logo} />
+
+    <div className='menu'>
+      <div className='slectedMenu'>
+        Works
+      </div>
+      <div className='defaultMenu'>
+        Writing
+      </div>
+      <div className='defaultMenu'>
+        About
+      </div>
     </div>
+      
+    </div>
+
   );
 }
 
