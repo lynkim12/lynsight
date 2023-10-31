dfbf35fc11ae01a026075634c9cf761f4fdc3c00
lynkim12 <lynkim12@gmail.com>
2023-11-01 01:29:37 +0900

    page design

--- src/Works.tsx
+++ src/Works.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import Navigator from './Navigation';
 import Content from './workContent';
+import thumbnail1 from './thumbnail1.png';
+import thumbnail2 from './thumbnail2.png';
+import thumbnail3 from './thumbnail3.png';
 
 function Works() {
     return (
       <>
       <Navigator/>
-      <div className='contentslist'>
+      <div className='workContentslist'>
         <div className='contents'>
-            <Content/>
-            <Content/>
-            <Content/>
+            <Content thumbnail={thumbnail1} projectTitle={'New Ads Platform for Expert'} projectDescription={'Designing an entirely new advertising platform for Mid- to Large-scale Advertisers'} projectSubtitle={'Karrot'} period={'2022.01. - 2022.08.'}/>
+            <Content thumbnail={thumbnail2} projectTitle={'First User Process Design'} projectDescription={'Onboarding process design on bookcovery to make activation.'} projectSubtitle={'Bookcovery'} period={'2020.05 - 2020.11'}/>
+            <Content thumbnail={thumbnail3} projectTitle={'Store UX/UI Design'} projectDescription={'Mobile store tap UX/UI design on Styleshare to maximize store tap using rates and purchase amout.'} projectSubtitle={'Styleshare'} period={'2020.10 - 2020.12'}/>
         </div>
         <div className='contents'>
-            <Content/>
-            <Content/>
-            <Content/>
-        </div>
-        <div className='contents'>
-            <Content/>
-            <Content/>
-            <Content/>
-        </div>
+           <Content thumbnail={thumbnail1} projectTitle={'New Ads Platform for Expert'} projectDescription={'Designing an entirely new advertising platform for Mid- to Large-scale Advertisers'} projectSubtitle={'Karrot'} period={'2022.01. - 2022.08.'}/>
+            <Content thumbnail={thumbnail2} projectTitle={'First User Process Design'} projectDescription={'Onboarding process design on bookcovery to make activation.'} projectSubtitle={'Bookcovery'} period={'2020.05 - 2020.11'}/>
+            <Content thumbnail={thumbnail3} projectTitle={'Store UX/UI Design'} projectDescription={'Mobile store tap UX/UI design on Styleshare to maximize store tap using rates and purchase amout.'} projectSubtitle={'Styleshare'} period={'2020.10 - 2020.12'}/>
+         </div>
       </div>
       </>
     )
--- src/workContent.tsx
+++ src/workContent.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import './App.css';
 import './style.css';
+import thumbnail1 from './thumbnail1.png';
 
-export function Content() {
+export function Content (props: {
+    thumbnail: string;
+    projectTitle: string;
+    projectDescription : string;
+    projectSubtitle : string;
+    period : string;
+}) {
   return (
-    <div className='content'>
-        <img className='thumbnail' />
-        <div className='contentsText'>
-            <div className='subtitle'>
-                Karrot  
+    <div className='project'>
+        <img className='projectThumbnail' src={props.thumbnail} />
+        <div className='projectContents'>
+            <div className='projectSubtitle'>
+                {props.projectSubtitle}
             </div>
-            <div className='title'>
-                New Ads Platform for Expert
+            <div style={{display : "flex", flexDirection : "column",gap:"10px"}}>
+                <div className='projectTitle'>
+                    {props.projectTitle}
+                </div>
+                <div className='projectDescription'>
+                    {props.projectDescription}
+                </div>
             </div>
-            <div className='description'>
-                Designing an entirely new advertising platform for mid- to large-scale advertisers
-            </div>
+            <div className='projectPeriod'>{props.period}</div>
         </div>
     </div>
   
