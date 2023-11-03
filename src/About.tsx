2a1d68452cd06c275cf8a4cf793ba44c4b018a32
Irene <irene@Karrot-Irene.local>
2023-11-04 00:02:57 +0900

    about font

--- src/About.tsx
+++ src/About.tsx
@@ -3,17 +3,28 @@ import Navigator from './Navigation';
 import { AboutList } from './AboutList';
 import { Divider } from '@mui/material';
 import './style.css';
+import bookcovery from './image/bookcovery.png'
+import daangn from './image/daangn.png'
+import styleshare from './image/styleshare.png'
 
 function About() {
     return (
       <>
+      <div>
       <Navigator />
-      <div className='aboutContents' style={{backgroundColor : "#000"}}>
-        <div className='aboutIntro' >Product Designer <br/> Lyn kim*</div>
-        <div className='aboutDescription'  style={{color : "#fff"}}>
-          Currently, I joined Karrot korea as a product designer contributing to the development of an advertising platform for small-sized business. I am responsible for advertiser's experience of creating ads and analyzing performance. We also have the goal to help for small-sized businesses to achieve the success through Karrot advertising.
+      <div className='aboutIntroSection'>
+        <div className='aboutIntro' >Product Designer <br/> Lyn Kim</div>
+        <div className='aboutIntroDescription'>
+          Currently, I joined Karrot korea as a product designer contributing to the development of an advertising platform for small-sized business. I am responsible for advertiser's experience of creating ads and analyzing performance. We also have a goal to help for small-sized businesses to achieve the success through Karrot advertising.
         </div>
-      </div>
+        
+        {/* <div>
+            <img className='companyLogo' src={bookcovery} style = {{position: 'absolute', left: '40px', top: '90px'}}/>
+            <img className='companyLogo' src={daangn} style = {{position: 'absolute', left: '780px', top: '-10px'}}/>
+            <img className='companyLogo' src={styleshare} style = {{position: 'absolute', left: '600px', top: '400px'}}/>
+        </div> */}
+     </div>
+     <Divider className='dividerDash'/>
       <div className='aboutContents'>
         <div className='aboutTitle'>Experience</div>
           <div className='aboutDescription' style={{marginBottom : "100px"}}>
@@ -70,6 +81,7 @@ function About() {
           <a href='mailto:lynkim12@gmail.com' target='_blank' className='aboutContactListItem'><div>lynkim12@gmail.com</div></a>
           <a href='https://www.linkedin.com/in/lyn-k-a0078316b/' target='_blank' className='aboutContactListItem'><div className='aboutContactListItem'>LinkedIn</div></a>
           <a href='https://brunch.co.kr/@lynkim' target='_blank' className='aboutContactListItem'><div>Brunch</div></a>
+        </div>
         </div>
         </div>
       </>
