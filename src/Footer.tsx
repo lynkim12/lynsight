7ab1ee5dd71bb88aa6dac541dc78675833cdfb56
Irene <irene@Karrot-Irene.local>
2023-11-04 22:13:44 +0900

    change linkedin url

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
         <div className='aboutTitle'>Contact</div>
         <div className='aboutContactList'>
           <a href='mailto:lynkim12@gmail.com' target='_blank' className='aboutContactListItem'><div>lynkim12@gmail.com</div></a>
-          <a href='https://www.linkedin.com/in/lyn-k-a0078316b/' target='_blank' className='aboutContactListItem'><div className='aboutContactListItem'>LinkedIn</div></a>
+          <a href='https://www.linkedin.com/in/lynkim12' target='_blank' className='aboutContactListItem'><div className='aboutContactListItem'>LinkedIn</div></a>
           <a href='https://brunch.co.kr/@lynkim' target='_blank' className='aboutContactListItem'><div>Brunch</div></a>
         </div>
         </div>
