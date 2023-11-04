b0077f2ce34e3058cdb7ec11837d31703ee648e3
Irene <irene@Karrot-Irene.local>
2023-11-04 18:34:47 +0900

    align

--- src/Waiting.tsx
+++ src/Waiting.tsx
@@ -5,7 +5,7 @@ import waiting from './image/waiting.png';
 export function Waiting() {
     return (
       <>
-      <div style ={{display:'block', padding: "240px 48px", textAlign: 'center'}}>
+      <div className='center-screen' >
       <img src={waiting} style={{width: "480px", aspectRatio: "auto", marginBottom: "28px"}}/>
       <div style ={{fontFamily:"Montserrat", fontWeight:"600", fontSize: "24px", color: "#131517"}}>
         Waiting for update
