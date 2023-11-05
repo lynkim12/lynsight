7faa4186946423195ac97f49c19dd8f2da414307
Irene <irene@Karrot-Irene.local>
2023-11-05 17:49:06 +0900

    add animation

--- src/Waiting.tsx
+++ src/Waiting.tsx
@@ -5,8 +5,8 @@ import waiting from './image/waiting.png';
 export function Waiting() {
     return (
       <>
-      <div className='center-screen' >
-      <img src={waiting} style={{width: "480px", aspectRatio: "auto", marginBottom: "28px"}}/>
+      <div className='center-screen fadeIn' >
+      <img src={waiting} className={"flow"} style={{width: "480px", aspectRatio: "auto", marginBottom: "28px"}}/>
       <div style ={{fontFamily:"Montserrat", fontWeight:"600", fontSize: "24px", color: "#131517"}}>
         Waiting for update
       </div>
