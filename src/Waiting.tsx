94ff809edef530e936901947db318854007027cd
Irene <irene@Karrot-Irene.local>
2023-12-03 12:02:33 +0900

    adjustment of the wating page margin

--- src/Waiting.tsx
+++ src/Waiting.tsx
@@ -5,7 +5,7 @@ import waiting from './image/waiting.png';
 export function Waiting() {
     return (
       <>
-      <div className='center-screen fadeIn' >
+      <div className='center-screen fadeIn' style={{marginTop: "-80px"}} >
       <img src={waiting} className={"flow"} style={{width: "480px", aspectRatio: "auto", marginBottom: "28px"}}/>
       <div style ={{fontFamily: "Handlee", fontSize: "28px", color: "#131517"}}>
         Waiting for update
