0becc19ace03418c70a476c0261a88918318fc90
Irene <irene@Karrot-Irene.local>
2024-07-21 20:01:33 +0900

    layout

--- src/Works.tsx
+++ src/Works.tsx
@@ -31,9 +31,9 @@ function Works(props: {
             <Content projectPage={"Work3"} thumbnail={thumbnail3} projectTitle={'Store UX/UI Design'} projectDescription={'Mobile store tap UX/UI design on Styleshare to maximize store tap using rates and purchase amout.'} projectSubtitle={'Styleshare'} period={'2020.10 - 2020.12'}/>
         </div>
         <div className='contents zoomIn'>
-           <Content projectPage={"Work5"} thumbnail={thumbnail5} projectTitle={'New Service Design'} projectDescription={'Designing an entirely new book-share platform for working people who want to develop compentency.'} projectSubtitle={'Karrot'} period={'2022.01. - 2022.08.'}/>
-            <Content projectPage={"Work2"} thumbnail={thumbnail2} projectTitle={'First User Process Design'} projectDescription={'Onboarding process design on bookcovery to make activation.'} projectSubtitle={'Bookcovery'} period={'2020.05 - 2020.11'}/>
-            <Content projectPage={"Work3"} thumbnail={thumbnail3} projectTitle={'Store UX/UI Design'} projectDescription={'Mobile store tap UX/UI design on Styleshare to maximize store tap using rates and purchase amout.'} projectSubtitle={'Styleshare'} period={'2020.10 - 2020.12'}/>
+          <Content projectPage={"Work5"} thumbnail={thumbnail5} projectTitle={'New Service Design'} projectDescription={'Designing an entirely new book-share platform for working people who want to develop compentency.'} projectSubtitle={'Karrot'} period={'2022.01. - 2022.08.'}/>
+          <Content projectPage={"Work2"} thumbnail={thumbnail2} projectTitle={'First User Process Design'} projectDescription={'Onboarding process design on bookcovery to make activation.'} projectSubtitle={'Bookcovery'} period={'2020.05 - 2020.11'}/>
+          <div style={{flex:'1'}}/>
          </div>
       </div>
       <Footer/>
