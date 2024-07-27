688e530d873bdd710834fba7781ac543ba9af6f2
Irene <irene@Karrot-Irene.local>
2024-07-27 12:36:46 +0900

    add contents in work6

--- src/Work6.tsx
+++ src/Work6.tsx
@@ -11,6 +11,7 @@ import work1chart1 from './image/works/work1chart1.png';
 import { Dispatch, SetStateAction, useEffect } from 'react';
 import wavedivider from './image/wavedivider.png'
 import { MainImage } from './workDetailPage/WorkMainImage';
+import expert from './image/works/work6_img_expert.png';
 
 //New Outlink-Type Ads
 
@@ -30,15 +31,24 @@ function Work6(props: {
           period={'2023.04 - 2023.07'}/>
       </div>
       <MainImage image={mainImage}/>
-      < WorkTextSection 
-        title={'Background'} 
-        description={
-          <div>
-            On Karrot, there are small advertisers who spend less than 1 million KRW per month on ads and professional advertisers who spend more than 1 million KRW. <br/> The expert mode has a campaign &gt; ad group &gt; ad structure, which is challenging for small business owners who are not familiar with advertising to understand and use effectively. 
-            <br/><br/> In the expert mode platform for professional advertisers, the "Outlink Ads" product is exclusively offered. We observed that many small advertisers are migrating to the expert mode to use "Outlink Ads." Recognizing the strong demand for a simple and easy-to-use outlink ad product on mobile, we decided to add the outlink ad product to the easy mode for advertising.
-          </div>} 
-      />
-      
+      <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce={true} offset={10}> 
+        < WorkTextSection 
+          title={'Background'} 
+          description={
+            <div>
+              On Karrot, there are small advertisers who spend less than 1 million KRW per month on ads and professional advertisers who spend more than 1 million KRW. <br/> The expert mode has a campaign &gt; ad group &gt; ad structure, which is challenging for small business owners who are not familiar with advertising to understand and use effectively. 
+              </div>} 
+        />
+        <div className='image-center-align'> 
+          <img style={{ width:"100%", aspectRatio: "auto", maxWidth:'1100px'}} src = {expert}/>
+        </div>
+        < WorkTextSection 
+          description={
+            <div>
+              In the expert mode platform for professional advertisers, the "Outlink Ads" product is exclusively offered. We observed that many small advertisers are migrating to the expert mode to use "Outlink Ads." Recognizing the strong demand for a simple and easy-to-use outlink ad product on mobile, we decided to add the outlink ad product to the easy mode for advertising.
+            </div>} 
+        />
+      </AnimationOnScroll>
       <Waiting/>
       </>
     )
--- src/workDetailPage/WorkTextSection.tsx
+++ src/workDetailPage/WorkTextSection.tsx
@@ -12,22 +12,20 @@ export function WorkTextSection(props: {
         <>
         <AnimationOnScroll animateIn="animate__fadeInUp" animateOnce={true} offset={10}>
             <div style={{ margin:"auto", maxWidth:"1400px", alignContent:"center", padding: "120px 48px", display: "flex", flexDirection: "column", gap: "28px" }}>
-                <div
-                    style={{
+                {props.title&& <div style={{
                         color: "#131517",
                         fontSize: "24px",
                         fontStyle: "normal",
                         fontWeight: "700",
-                        lineHeight: "normal",
-                    }}>{props.title}</div>
-                {props.description &&<div style={{
+                        lineHeight: "normal"
+                    }}>{props.title}</div>}
+                {props.description && <div style={{
                     color: "#3A3E44",
                     fontSize: "20px",
                     fontStyle: "normal",
                     fontWeight: "400",
-                    lineHeight: "151.8%",
-                }}>{props.description}
-                </div>}
+                    lineHeight: "151.8%"
+                }}>{props.description}</div>}
             </div>
         </AnimationOnScroll>
         </>
