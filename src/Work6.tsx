cf471c7311256e1e402b8879b0d293f027e6244a
Irene <irene@Karrot-Irene.local>
2024-08-18 15:01:52 +0900

    work6 update

--- src/Work6.tsx
+++ src/Work6.tsx
@@ -18,7 +18,10 @@ import survey from './image/works/work6survey.png';
 import ecosystem  from './image/works/work6ecosystem.png';
 import direction  from './image/works/work6direction.png';
 import BackButton from './components/BackButton';
-
+import userflow from './image/works/work6userflow.png';
+import research1 from './image/works/work6research1.png';
+import research2 from './image/works/work6research2.png';
+import research3 from './image/works/work6research3.png';
 
 //New Outlink-Type Ads
 
@@ -51,7 +54,20 @@ function Work6(props: {
               In the expert mode platform for professional advertisers, the "Outlink Ads" product is exclusively offered. We observed that many small advertisers are migrating to the expert mode to use "Outlink Ads." Recognizing the strong demand for a simple and easy-to-use outlink ad product on mobile, we decided to add the outlink ad product to the easy mode for advertising.
           </div>}/>
         <WorkImage image={chart1} isMaxheight={true} isbottomPadding={true} />
-        <WorkFullImage image={survey} isRoundedCorner={false} isBackgroundColor={false} isMargin={true}  />
+        <WorkFullImage image={survey} isRoundedCorner={false} isBackgroundColor={false} isMargin={true} isBottomMargin={true} />
+        
+        <WorkTextSection 
+          title={'User Research'} 
+          description={<div>
+              On Karrot, there are small advertisers who spend less than 1 million KRW per month on ads and professional advertisers who spend more than 1 million KRW. <br/> The expert mode has a campaign &gt; ad group &gt; ad structure, which is challenging for small business owners who are not familiar with advertising to understand and use effectively. 
+            </div>} 
+        />
+        <div className='image-center' style={{display:'flex', flexDirection:'row', gap:'24px', margin:'auto', maxWidth:'1400px'}}>
+        <WorkFullImage image={research1} isMargin={false} isBackgroundColor={true} isRoundedCorner={true} isBottomMargin={true} description='Example of a Post'/>   
+        <WorkFullImage image={research2} isMargin={false} isBackgroundColor={true} isRoundedCorner={true} isBottomMargin={true} description='Share by Outlink Type'/>   
+        <WorkFullImage image={research3} isMargin={false} isBackgroundColor={true} isRoundedCorner={true} isBottomMargin={true} description='Outlink by Usecase'/>   
+        </div>
+
         <WorkTextSection 
           title={'Goal'} 
           description={<div>
@@ -69,7 +85,10 @@ function Work6(props: {
           title={'Design Direction'} 
           description={<div>
               Unlike outlink ads provided to professional advertisers, the UX direction for outlink ads aimed at small advertisers has been set to ensure that anyone can use them easily.</div>}/>
-        <WorkFullImage image={direction} isRoundedCorner={false} isBackgroundColor={false} isMargin={true}/>
+        <WorkFullImage image={direction} isRoundedCorner={false} isBackgroundColor={false} isBottomMargin={true} isMargin={true}/>
+      <WorkTextSection title='UXUI Design'/>
+      <WorkFullImage image={userflow}/>
+      
       </AnimationOnScroll>
       <Waiting/>
       <BackButton/>
