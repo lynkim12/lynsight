16f215e505ce3ce3fd7a0b23c1c41ee24ffff1cf
Irene <irene@Karrot-Irene.local>
2024-08-25 11:23:24 +0900

    work5 update

--- src/Work5.tsx
+++ src/Work5.tsx
@@ -7,7 +7,6 @@ import {WorkTitle} from './workDetailPage/WorkTitle';
 import mainImage from './image/works/work5MainImage.png';
 import { WorkTextSection } from './workDetailPage/WorkTextSection';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-import work1chart1 from './image/works/work1chart1.png';
 import { Dispatch, SetStateAction, useEffect } from 'react';
 import wavedivider from './image/wavedivider.png'
 import { MainImage } from './workDetailPage/WorkMainImage';
@@ -17,6 +16,11 @@ import research1 from './image/works/work5research1.png';
 import research2 from './image/works/work5research2.png';
 import { Divider } from '@mui/material';
 import { Wavedivider } from './Wavedivider';
+import strategy from './image/works/work5strategy.png';
+import { WorkImage } from './workDetailPage/WorkImage';
+import wireframe from './image/works/work5wireframe.png';
+import system from './image/works/work5system.png';
+import system2 from './image/works/work5system2.png';
 
 //New Service Design
 
@@ -48,8 +52,17 @@ function Work5(props: {
         description={<div>We recruited approximately 40 beta testers in their 20s to 40s. These beta testers are professionals who study specialized knowledge through books, work in specialized fields, and write about their expertise. <br/> They expressed key needs for a sense of <b>"intellectual belonging" and the "continuous discovery of learning materials."</b></div>}/>
       <WorkFullImage image={research1}/>
       <WorkFullImage image={research2}/>
-
-
+      <WorkTextSection
+        title='Hooks Habituation Strategy'
+        description='Based on user interviews, we categorized users by their key needs and developed core service features as well as strategies for activation and retention.'/>
+      <WorkImage image={strategy} isMaxheight={false} isbottomPadding={true}/>
+      <div style={{background: 'linear-gradient(180deg, #F4F5F8 0%, #F1F2F7 100%)', paddingBottom:'40px'}}>
+        <WorkTextSection title='Wireframe'/>
+        <WorkFullImage image={wireframe} isBottomMargin={true} /> 
+      </div>
+      <WorkTextSection title='Design System'/>
+      <WorkFullImage image={system}/>
+      <WorkFullImage image={system2}/>
       <Waiting/>
       <BackButton/>
       </>
