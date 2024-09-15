be919fe0d8427213c95c032d5339418712ed3856
Irene <irene@Karrot-Irene.local>
2024-09-15 14:23:58 +0900

    work10 update

--- src/Work10.tsx
+++ src/Work10.tsx
@@ -21,6 +21,11 @@ import problem1 from './image/works/work10problem1.png';
 import problem2 from './image/works/work10problem2.png';
 import problem3 from './image/works/work10problem3.png';
 import problem4 from './image/works/work10problem4.png';
+import solution1 from './image/works/work10solution1.png';
+import solution2 from './image/works/work10solution2.png';
+import revise from './image/works/work10 Revised structure.png';
+import past from './image/works/work10 Past structure.png';
+import solution11 from './image/works/work10solution1-1.png';
 import { Divider } from '@mui/material';
 
 //Eliminating Concerns of Creating Ads
@@ -44,16 +49,31 @@ function Work10(props: {
       <Divider/>
       <WorkTextSection title='Background' description='Among the advertisers who have created an ad account, 36% have never created an ad. To encourage them to create ads, we analyzed the issues with the current design and proposed various ideas.'/>
       <WorkFullImage image={background}  isMargin={true} isBottomMargin={true}/>
-      <WorkTextSection title='Problem 1.' description={'The rate of clicking on "Create Ad" was very low at 29%, and the completion rate for those who attempted to create an ad was also very low at 34%.'}/>
-      <div className={'margin48'} style={{margin:"auto", maxWidth:"1600px", alignContent: "center", display: "flex", flexDirection: "row", gap:"40px", marginBottom:'60px'}}>
-          <WorkFullImage image={problem1} />     
-          <WorkFullImage image={problem2} />        
+      <div style={{backgroundColor:'#F4F5F8'}}>
+        <WorkTextSection title='Problem 1.' description={'The rate of clicking on "Create Ad" was very low at 29%, and the completion rate for those who attempted to create an ad was also very low at 34%.'}/>
+        <div className={'margin48'} style={{margin:"auto", maxWidth:"1600px", alignContent: "center", display: "flex", flexDirection: "row", gap:"40px", marginBottom:'60px'}}>
+            <WorkFullImage image={problem1} />     
+            <WorkFullImage image={problem2} />        
+        </div>
+        <WorkTextSection title='Problem 2.' description={"After drafting ad posts, most advertisers have launched their ads within 1 to 2 days. This indicates that advertisers decide on whether to run an ad within 2 days of creating the post. It's important to reflect this behavior pattern when selecting ad creatives."}/>
+        <div className={'margin48'} style={{margin:"auto", maxWidth:"1600px", alignContent: "center", display: "flex", flexDirection: "row", gap:"40px", marginBottom:'120px'}}>
+            <WorkFullImage image={problem3} />     
+            <WorkFullImage image={problem4} />        
+        </div>
+      <Divider/>
       </div>
-      <WorkTextSection title='Problem 2.' description={"After drafting ad posts, most advertisers have launched their ads within 1 to 2 days. This indicates that advertisers decide on whether to run an ad within 2 days of creating the post. It's important to reflect this behavior pattern when selecting ad creatives."}/>
+      <WorkTextSection title='Solution 1.' description={"Encouraging ad creation on empty page"}/>
+      <WorkFullImage image={solution1} />   
+      <WorkFullImage image={solution11} /> 
+      <Divider/>
+      <WorkTextSection title='Solution 2.' description={"Recommend ad creatives during the selection phase"}/>
+      <WorkFullImage image={solution2} isBottomMargin={true} />   
       <div className={'margin48'} style={{margin:"auto", maxWidth:"1600px", alignContent: "center", display: "flex", flexDirection: "row", gap:"40px", marginBottom:'60px'}}>
-          <WorkFullImage image={problem3} />     
-          <WorkFullImage image={problem4} />        
-      </div>
+            <WorkFullImage image={past} isBorder={true} isRoundedCorner={true} isShadow={true} />     
+            <WorkFullImage image={revise} isBorder={true} isRoundedCorner={true} isShadow={true}/>        
+        </div>
+
+
       <Waiting/>
       <BackButton/>
       <NextItem to='/Work1' thumbnail={thumbnail1} nextProjectTitle='New Ads Platform for Experts'/>
